fbcb9a15aac7dd89286fbf3b2e3776c9da98cfed
DeguzBelarus <deguz@mail.ru>
2023-06-30 14:43:42 +0300

    feat: update scripts and message on server start in dev or prod mode

--- server.ts
+++ server.ts
@@ -8,5 +8,9 @@ const PORT = process.env.PORT || 4000;
 const server = http.createServer(router);
 
 server.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}...`);
+  if (process.env.NODE_ENV === 'production') {
+    console.log(`Server has been started on port ${PORT}...`);
+  } else {
+    console.log(`Server has been started on port ${PORT} in development mode...`);
+  }
 });
